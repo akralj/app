922ea0ae526ac7d670a4782bf3c863eb78a9c52d
akralj <dev@kralj.de>
2017-12-05 11:55:09 +0100

    add client again

---
+++ client/src/app.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+
+v-app#inspire
+  v-navigation-drawer(app, fixed, v-model='drawer', :mobileBreakPoint="900")
+    v-list()
+      v-list-tile(@click='')
+        v-list-tile-avatar
+          img(src="https://randomuser.me/api/portraits/men/85.jpg")
+        v-list-tile-content
+          v-list-tile-title Titel
+    v-list.pt-0
+      v-divider
+      v-list-tile(v-for="item in menuItems", :key="item.title", :to="item.link")
+        v-list-tile-action
+          v-icon {{ item.icon }}
+        v-list-tile-content
+          v-list-tile-title {{ item.title }}
+
+  v-toolbar.indigo(dark, fixed, app)
+    v-toolbar-side-icon(@click.stop='drawer = !drawer')
+    v-toolbar-title {{$route.name}}
+
+  router-view(:key="$route.path")
+
+</template>
+
+
+<script lang="coffee">
+
+exports.default =
+  data: ->
+    {
+      drawer: true
+      menuItems: [
+        { title: 'Home', icon: 'dashboard'      , link: '/' }
+        { title: 'User', icon: 'question_answer', link: '/user' }
+      ]
+      mini: false
+      right: null
+    }
+
+</script>
+
+
+<style src='vuetify/dist/vuetify.min.css'></style>
+<style lang="stylus">
+  //@import './stylus/main'
+</style>
---
+++ client/src/index.coffee
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+param = require("jquery-param")
+window._ = require('lodash')
+
+import app    from './app.vue'
+import router from './router'
+
+new Vue({
+  el: '#app',
+  router: router,
+  render: (h) -> h(app)
+})
